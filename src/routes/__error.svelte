fad091b8afc0f850b4f726ea5ffc6396ebd8028b
JustinVx <github@justinvenbrux.nl>
2021-12-15 12:03:43 +0100

    Add 404 error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,13 @@
+<script>
+
+import { exclude_internal_props } from "svelte/internal";
+
+</script>
+
+
+<div class="max-w-none page mt-8 mx-auto py-4 sm:py-4">
+  <p>I'm sorry. This page does not exist.</p>
+  <div class="max-w-xs mt-8">
+    <img src="/img/A404.png" alt="Floating astronaut"/>
+  </div>
+</div>
